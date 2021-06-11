074c33c338402a926cd6a3888e60384d7b3bab84
MatteoCrew <72408570+MatteoCrew@users.noreply.github.com>
2021-06-11 16:48:10 -0300

    Update Empresa.java

--- source/data/Empresa.java
+++ source/data/Empresa.java
@@ -2,5 +2,7 @@ package data;
 
 public abstract class Empresa
 {
-
+	private String nombre; //nombre de la empresa
+	private String CEO; //nombre del jugador
+	private double patrimonio; //patrimonio de la empresa
 }
