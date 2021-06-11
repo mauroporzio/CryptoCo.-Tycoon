6f3a025dc9f39324dd761c9fab666d5de7f8c329
MatteoCrew <72408570+MatteoCrew@users.noreply.github.com>
2021-06-11 16:49:54 -0300

    Update EmpresaEnemiga.java

--- source/data/EmpresaEnemiga.java
+++ source/data/EmpresaEnemiga.java
@@ -2,5 +2,6 @@ package data;
 
 public class EmpresaEnemiga extends Empresa implements IProbabilidad
 {
-
+	private int dificultad; //valor que se utiliza para conocer la dificultad de la empresa enemiga. 1 fácil, 2 medio, 3 difícil. Afecta el patrimonio.
+	
 }
