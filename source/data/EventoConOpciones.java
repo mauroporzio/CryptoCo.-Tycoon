65ac5d6bec0f5771bfa984b05a6450f2e2cad2e0
MatteoCrew <72408570+MatteoCrew@users.noreply.github.com>
2021-06-11 16:47:04 -0300

    Update EventoConOpciones.java

--- source/data/EventoConOpciones.java
+++ source/data/EventoConOpciones.java
@@ -1,6 +1,9 @@
 package data;
 
+import java.util.ArrayList;
+
 public class EventoConOpciones extends Evento
 {
-
+	private ArrayList<String> opciones; //almacén para los strings de las opciones del evento, se utilizan arreglos paralelos para almacenar las opcion y los datos correspondientes
+	private ArrayList<Integer> datos; //almacén para los datos correspondientes con las opciones, se utilizan arreglos paralelos para almacenar las opcion y los datos correspondientes
 }
