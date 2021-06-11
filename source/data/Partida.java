c82006aa81c5a92a754231242ee37935fb14f684
MatteoCrew <72408570+MatteoCrew@users.noreply.github.com>
2021-06-11 17:28:45 -0300

    Update Partida.java

--- source/data/Partida.java
+++ source/data/Partida.java
@@ -7,4 +7,35 @@ public class Partida implements IProbabilidad
 	private ArrayList<Evento> eventos;
 	private ArrayList<EmpresaEnemiga> competencia;
 	private EmpresaUsuario empresa;
+	
+	
+	
+	
+	public Partida(ArrayList<Evento> eventos, ArrayList<EmpresaEnemiga> competencia, EmpresaUsuario empresa)  //constructor para partida cargada.
+	{
+		this.eventos = new ArrayList<Evento>();
+		this.competencia = new ArrayList<EmpresaEnemiga>();
+		this.eventos = eventos;
+		this.competencia = competencia;
+		this.empresa = empresa;
+	}
+	
+	public Partida() //constructor para nueva partida.
+	{
+		this.eventos = new ArrayList<Evento>();
+		this.competencia = new ArrayList<EmpresaEnemiga>();
+		this.empresa = new EmpresaUsuario();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+
