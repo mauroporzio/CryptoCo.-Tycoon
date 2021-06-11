f855e14f8ef808b4266b380bd4d1eae3ced7e3ac
MatteoCrew <72408570+MatteoCrew@users.noreply.github.com>
2021-06-11 17:18:44 -0300

    Update Modificador.java

--- source/data/Modificador.java
+++ source/data/Modificador.java
@@ -2,5 +2,10 @@ package data;
 
 public class Modificador 
 {
-
+	private double valor; //Suma o resta el contenido. Afecta directamente al patrimonio.
+	private int porcentaje; //Modifica las ganancias mensuales finales sumando o restando un porcentaje. Valor máximo ??
+	private String nombre; //Nombre del modificador.
+	private String descripcion; //Descripción del modificador.
+	
+	//se utilizarán dos constructores, uno con porcentajes y otro con valores. Si se trabaja con uno se ignora el otro.
 }
