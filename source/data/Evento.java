c109830e53033897ed73ed49c8180605870568d5
nicolasbrun333 <70588511+nicolasbrun333@users.noreply.github.com>
2021-06-15 21:17:48 -0300

    Update Evento.java

--- source/data/Evento.java
+++ source/data/Evento.java
@@ -6,4 +6,9 @@ public class Evento implements IProbabilidad
 	private String nombre;
 	private String descripcion;
 	private boolean eventoActivo; //(true: activo; false: inactivo) al existir eventos que pueden mantenerse activos por cierto tiempo, no pueden aparecer nuevamente mientras estén activos.
+	
+	
+	@Override
+	public int calcularProbabilidad() { return (int)Math.floor(Math.random()*(10-0+1)+0)*10; }	//Devuelve un numero entre 0 y 100 en saltos de 10
+		
 }
